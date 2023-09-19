c6b42ad5ec7bd45d922cfd77f0c9d92866723456
europium <62637944+eeuropium@users.noreply.github.com>
2023-09-19 12:10:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ version 1.1.4
 changes:
     - authentication feature
     - new character Farohar
-    - replaced key function with new "orbing lights" key function
+    - replaced key function with new key function
     - reorganised code for character selection for easier adding of functionalities in the future
     - hosted as part of KL coding cup (KL coding cup code is removed, backed-up currently)
 '''
