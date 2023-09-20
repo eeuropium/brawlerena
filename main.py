3e68e37a28b9f145a38b05e01e4e7df01b44c492
europium <62637944+eeuropium@users.noreply.github.com>
2023-09-20 20:14:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,10 @@ changes:
     - new character Farohar
     - replaced key function with new key function
     - reorganised code for character selection for easier adding of functionalities in the future
-    - hosted as part of KL coding cup (KL coding cup code is removed, backed-up currently)
+    - hosted as part of KL coding cup
 '''
+
+
+# Developer A: I heard that there is a secret password for Q8 somewhere here.
+# Developer B: Oops. I think I deleted it with a new commit ...
+# Developer A: What?! cd backup.py
